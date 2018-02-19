362cb11ee924ec0e1e461f122ea538fc249f2d09
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-02-19 17:20:54 +1100

    Added Javascript folder and first excercise

---
+++ javascript/stalker.js
@@ -0,0 +1,6 @@
+const firstName = prompt('Please enter your First Name');
+const lastName = prompt('And what is your Last Name?');
+const age = prompt('How old are you?');
+
+alert('Hi ' + firstName + ' ' + lastName + '. How are you today?');
+console.log('You are ' + age + ' years old huh?');
