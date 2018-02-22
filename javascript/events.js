7312298a9e6091aa1e7a5a0e4797b0c8cc73730a
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-02-22 17:50:04 +1100

    Tuning

--- javascript/events.js
+++ javascript/events.js
@@ -1,9 +1,8 @@
 var button = document.querySelector('button');
-var body = document.querySelector('body');
 button.addEventListener("click", function() {
 	if(body.style.backgroundColor === "purple") {
-		body.style.backgroundColor = "white";
+		document.body.background = "white";
     } else {
-		body.style.backgroundColor = "purple";
+		document.body.background = "purple";
     }
 })
