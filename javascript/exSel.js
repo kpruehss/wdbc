960ecfcf28af57c1580b92d0528c2677718d7f90
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-02-22 17:14:27 +1100

    Continued working on Javascript section

---
+++ javascript/exSel.js
@@ -0,0 +1,7 @@
+const fP1 = document.getElementById('p');
+
+const fp2 = document.getElementsByClassName('special')[0];
+
+const fp3 = document.querySelector('p');
+
+const fp4 = document.querySelector('#first');
---
+++ javascript/objects-copy.js
@@ -0,0 +1,32 @@
+const movieDB = [
+    movie1 = {
+        title: 'Forest Gump',
+        hasWatched: true,
+        rating: '5 Stars',
+    },
+    movie2 = {
+        title: 'Top Gun',
+        hasWatched: true,
+        rating: '5 Stars',
+    },
+    movie3 = {
+        title: 'Frozen',
+        hasWatched: false,
+        rating: '2 Stars',
+    },
+    movie4 = {
+        title: 'The Red Pill',
+        hasWatched: false,
+        rating: '4 stars',
+    }
+];
+
+function printMovies(movie) {
+    if (movie.hasWatched) {
+        console.log('You have watched "' + movie.title + '" - ' + movie.rating);
+    } else {
+        console.log('You have not watched "' + movie.title + '" - ' + movie.rating);
+    }
+}
+
+movieDB.forEach(printMovies);
