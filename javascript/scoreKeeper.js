587b14dd7176beadca9c3d82555ea8bfcf77d29e
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-06 20:50:42 +1100

    Rewrote scoreKeeper.js

--- javascript/scoreKeeper.js
+++ javascript/scoreKeeper.js
@@ -1,28 +1,40 @@
-const p1Button = document.querySelector('#p1');
-const p2Button = document.querySelector('#p2');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const reset = document.getElementById('reset');
-const scoreSelect = document.getElementById('scoreSelect');
-const winScore = document.querySelector('#winScore');
-var p1Score = 0;
-var p2Score = 0;
+const p1Button = document.querySelector("#p1"),
+    p1Display = document.querySelector("#p1Display"),
+    p2Button = document.querySelector("#p2"),
+    p2Display = document.querySelector("#p2Display"),
+    resetButton = document.querySelector("#reset");
 
-p1Button.addEventListener('click', function() {
-  p1Score++;
-  p1Display.textContent = p1Score;
+let gameOver = false,
+    p1Score = 0,
+    p2Score = 0,
+    winningScore = 0;
+
+p1Button.addEventListener("click", () => {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            alert("Player 1 won!");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
 });
 
-p2Button = addEventListener('click', function() {
-  p2Score++;
-  p2Display.textContent = p2Score;
+p2Button.addEventListener("click", () => {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            alert("Player 2 won!");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 });
 
-reset = addEventListener('click', function() {
-  p1Display.textContent = 0;
-  p1Score = 0;
-  p2Display.textContent = 0;
-  p2Score = 0;
+resetButton.addEventListener("click", () => {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    gameOver = false;
 });
-
-winScore = scoreSelect.value;
