6265362926fa8151fa4f605253fd207dea9a8702
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-07 19:54:32 +1100

    Refactored reset functionality and adjusted Score Counter logic

--- javascript/scoreKeeper.js
+++ javascript/scoreKeeper.js
@@ -1,13 +1,25 @@
-var p1Button = document.querySelector('#p1'),
+var numInput = document.querySelector('input'),
+  p1Button = document.querySelector('#p1'),
   p1Display = document.querySelector('#p1Display'),
   p2Button = document.querySelector('#p2'),
   p2Display = document.querySelector('#p2Display'),
-  resetButton = document.querySelector('#reset');
+  resetButton = document.querySelector('#reset'),
+  scoreToWin = document.querySelector('p span');
 
 var gameOver = false,
   p1Score = 0,
   p2Score = 0,
   winningScore = 5;
+
+var resetFunc = function resetFunc () {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p1Display.classList.remove('winner');
+  p2Display.textContent = p2Score;
+  p2Display.classList.remove('winner');
+  gameOver = false;
+};
 
 p1Button.addEventListener('click', () => {
   if (!gameOver) {
@@ -32,11 +44,11 @@ p2Button.addEventListener('click', () => {
 });
 
 resetButton.addEventListener('click', () => {
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = p1Score;
-  p1Display.classList.remove('winner');
-  p2Display.textContent = p2Score;
-  p2Display.classList.remove('winner');
-  gameOver = false;
+  resetFunc();
+});
+
+numInput.addEventListener('change', () => {
+  scoreToWin.textContent = numInput.valueAsNumber;
+  winningScore = numInput.valueAsNumber;
+  resetFunc();
 });
