393c9616048ed64bd94a2d39a3f6c30afdaff11e
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-08 17:09:14 +1100

    Added core logic to JS file

---
+++ Projects/ColorGame/colorGame.js
@@ -0,0 +1,33 @@
+let colors = [
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 255)'
+  ],
+  pickedColor = colors[3];
+
+const colorDisplay = document.getElementById('colorDisplay'),
+  messageDisplay = document.querySelector('#message'),
+  squares = document.querySelectorAll('.square');
+
+colorDisplay.textContent = pickedColor;
+
+for (square in squares) {
+  // Add initial color to squares
+  squares[square].style.backgroundColor = colors[square];
+  // Add event listener to each square
+  squares[square].addEventListener('click', function () {
+    // Grab the color of clicked square
+    const clickedColor = this.style.backgroundColor;
+    // Compare color to clickedColor
+
+    if (clickedColor === pickedColor) {
+      alert('Correct!');
+    } else {
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = 'Try Again';
+    }
+  });
+}
